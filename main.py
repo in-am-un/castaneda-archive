8a0120ac859f02ae4db95335c8fa16508f706fc8
In Am Un <169553174+in-am-un@users.noreply.github.com>
2024-05-15 22:18:52 +0200

    download media from posts and comments, and some fancy progress reporting. still some fixes left required for media, notably on giphy and a few edge-case posts not working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import pprint
 from datetime import datetime
 from pathlib import Path
 from slugify import slugify
@@ -7,6 +8,7 @@ from typing import List, Dict, Optional
 import random
 import time
 import json
+from tqdm import tqdm
 
 
 SUBREDDIT_NAME = 'castaneda'
@@ -50,13 +52,14 @@ def get_post_ids_from_scraping(stop_at_id=None) -> List[str]:
     print(f"scraping post ids of /r/{SUBREDDIT_NAME}")
     print("-" * 33)
     def fetch_loop():
-        current_after_id = ""
+        current_after_id = stop_at_id or ""
         while posts := get_posts(current_after_id):
             for i, post in enumerate(posts):
                 if stop_at_id is not None and post['id'] == stop_at_id:
                     print("reached stop_at_id thats it folks thx")
                     return
-                print(post['id'], post['title'])
+                if not post['stickied']:
+                    print(post['id'], post['title'])
                 ids.append(post['id'])
             current_after_id = posts[-1]['name']  # query only works with 'name' field and not 'id'
     fetch_loop()
@@ -128,7 +131,7 @@ def get_post(post_id) -> Optional[Dict]:
     post_url = f"https://reddit.com/r/{SUBREDDIT_NAME}/comments/{post_id}.json"
     post_json = get_json(post_url)
     if not post_json:
-        print("woopie doopie something is spooky could not get json for post:", post_id)
+        tqdm.write("woopie doopie something is spooky could not get json for post:", post_id)
         return None
     return post_json
  
@@ -137,6 +140,118 @@ def datestamp_from_timestamp(timestamp: int) -> str:
     d = datetime.fromtimestamp(timestamp)
     return d.strftime("%Y%m%d%H%M%S")
 
+
+def get_archive() -> List[Dict]:
+    all_posts = []
+    for post in sorted(ARCHIVE_DIR.rglob('*.json')):
+        post_dict = {}
+        with open(post, 'r') as p:
+            post_dict = json.loads(p.read())
+            all_posts.append(post_dict)
+    return all_posts
+
+
+def list_chunks(l, n): 
+    for i in range(0, len(l), n):  
+        yield l[i:i + n]
+
+
+def download_archive_media(archive: List[Dict]):
+    import asyncio
+    import aiohttp
+
+    # image does not readily block on too many requests so here is fine to use async cocurrency
+    async def fetch_media():
+        async with aiohttp.ClientSession() as session:
+            progress = tqdm(
+                desc="download_archive_media",
+                total=len(archive),
+                position=0
+            )
+            with progress:
+                for pos, posts in enumerate(list_chunks(archive, 100)):
+                    async def dl(*args):
+                        await download_post_media(*args)
+                        progress.update(1)
+                    await asyncio.gather(*[dl(session, post) for post in posts])
+    asyncio.run(fetch_media())
+
+
+async def download_post_media(session, post):
+    meta = post[0]['data']['children'][0]['data']
+    datestamp = datestamp_from_timestamp(meta['created'])
+    post_media = meta.get('media_metadata') or {}
+    
+    def download_comments(comments: List[Dict]):
+        for comment in comments:
+            comment_media = comment.get('media_metadata')
+            if comment_media is not None:
+                for media_id in comment_media:
+                    post_media[media_id] = {
+                        'comment_id': comment['id'],
+                        'comment_permalink': comment['permalink'],
+                        'comment_url': f"https://reddit.com{comment['permalink']}",
+                        **comment_media[media_id]
+                    }
+            if comment.get('replies'):
+                download_comments(c['data'] for c in comment['replies']['data']['children'])
+
+    download_comments([c['data'] for c in post[1]['data']['children']])
+    for idx, media_id in enumerate(post_media):
+        media = post_media[media_id]
+        media_kind = media['e']
+        if media_kind in ["Image", "AnimatedImage"]:
+            img_kind = media['m']
+            if img_kind == "image/jpg":
+                ext = "jpg"
+            elif img_kind == "image/jpeg":
+                ext = "jpeg"
+            elif img_kind == "image/png":
+                ext = "png"
+            elif img_kind == "image/gif":
+                ext = "gif"
+            if external := media.get("ext"):
+                url = external
+                if "giphy" in external:
+                    #url = url.replace("https://giphy.com/gifs/", "https://giphy.com/embed/")
+                    continue
+            else:
+                url = f"https://i.redd.it/{media_id}.{ext}"
+        elif media_kind == "RedditVideo":
+            #dash_url = media[media_id]['dashUrl']
+            #video_id = dash_url.split('/')[6]
+            ext = "mp4"
+            size = media['y']
+            url = f"https://v.redd.it/{media['id']}/DASH_{size}.mp4"
+        else:
+            print("not handled:", media_kind, datestamp, meta['id'])
+            continue
+        if comment_id := media.get('comment_id'):
+            media_id = f"{comment_id}_{media_id}"
+        permalink = media.get('comment_permalink') or meta["permalink"]
+        media_path = f"{str(ARCHIVE_DIR)}/{datestamp}_{meta['id']}_{media_id}.{ext}"
+        if Path(media_path).exists():
+            continue
+
+        async with session.get(url, headers=HEADERS) as r:
+            if r.status != 200:
+                #tqdm.write(f"{[r.status]} media download failed: https://old.reddit.com{permalink}")
+                tqdm.write(f"{[r.status]} media download failed: {url}")
+                tqdm.write(pprint.pformat(media, indent=4))
+                continue
+            size = int(r.headers.get('content-length', 0)) or None
+            progress = tqdm(
+                desc=media_path,
+                total=size,
+                unit='B',
+                unit_scale=True,
+                leave=False
+            )
+            with open(media_path, 'wb') as f, progress:
+                async for chunk in r.content.iter_chunked(512):
+                    f.write(chunk)
+                    progress.update(len(chunk))
+        
 
 # json files are saved as "<DATESTAMP>_<POST_ID>_post-title-without-spaces-and-possibly-truncated.json"
 def save_post(post) -> str:
@@ -165,15 +280,29 @@ def main():
         ids_to_archive = set(ids_from_scraping) - set(ids_in_archive)
     else:
         print("err: fetch mode must be either 'index' or 'scrape'")
-        sys.exit()
+        return sys.exit()
+    if len(ids_to_archive) == 0:
+        print("nothing new to archive. bye.")
+        return sys.exit()
 
     print(f"fetching {len(ids_to_archive)} posts not yet archived")
-    for num, post_id in enumerate(ids_to_archive):
-        post = get_post(post_id)
-        filename = save_post(post)
-        print(f"[{num + 1}/{len(ids_to_archive)}] {filename}")
+    progress = tqdm(
+        desc="posts",
+        total=len(ids_to_archive),
+        position=0,
+    )
+    with progress:
+        for num, post_id in enumerate(ids_to_archive):
+            post = get_post(post_id)
+            filename = save_post(post)
+            tqdm.write(filename)
+            progress.update(1)
 
 
 if __name__ == '__main__':
-    main()
+    #main()
+    archive = get_archive()
+    archive.reverse()
+    download_archive_media(archive)
+
 
